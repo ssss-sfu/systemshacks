7768445b7a69b5e9b65a98688315f8b18c080ad7
Kiaan Castillo <kiaancastillo@gmail.com>
2019-11-10 21:11:03 -0800

    added navbar hide on scroll down

---
+++ lifehacks2020/scripts/navbar_hide_scroll.js
@@ -0,0 +1,18 @@
+let prevScrollpos = window.pageYOffset;
+let navbar = document.getElementById("nav");
+
+window.onscroll = function () {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    navbar.classList.add("nav--scrolled");
+  } else if (document.body.scrollTop <= 80 || document.documentElement.scrollTop <= 80) {
+    navbar.classList.remove("nav--scrolled");
+  }
+
+  let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    navbar.style.top = "0";
+  } else {
+    navbar.style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+}
